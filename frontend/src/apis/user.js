f87f6d5dc9fa7a5437a85328fb4482b7547317a1
zy7y <13271962515@163.com>
2021-10-04 14:59:19 +0800

    style: 格式化代码

--- frontend/src/apis/user.js
+++ frontend/src/apis/user.js
@@ -24,8 +24,8 @@ export function updateUser(nickname) {
     method: "put",
     data: {
       nickname,
-      password:'',
-      username: ''
-    }
+      password: "",
+      username: "",
+    },
   });
 }
