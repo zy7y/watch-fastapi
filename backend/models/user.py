b3e928c6e63cef4576f4f648a178351eba13f856
zy7y <13271962515@163.com>
2021-12-01 10:52:04 +0800

    fix: username unique true
    用户名需要唯一

--- backend/models/user.py
+++ backend/models/user.py
@@ -7,7 +7,7 @@ from core import get_password_hash
 
 
 class User(models.Model):
-    username = fields.CharField(max_length=20, null=False, description="账号")
+    username = fields.CharField(max_length=20, null=False, description="账号", unique=True)
     password = fields.CharField(max_length=128, null=False, description="密码")
     nickname = fields.CharField(max_length=20, null=True, description="昵称", default="你好")
 
